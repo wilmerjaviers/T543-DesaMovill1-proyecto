e71a4d736c58f1b15b2f7b0e2a0bb5293c76c074
wilmer_sanchez <wilmerjaviers@yahoo.com>
2025-09-16 19:24:29 -0600

    correction

--- tienda_asistencias_backend/routes/tiendaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getTiendas, createTienda } = require("../controllers/tiendaController");
-const verifyToken = require("../middleware/authMiddleware");
-
-router.get("/", verifyToken(["superadmin"]), getTiendas);
-router.post("/", verifyToken(["superadmin"]), createTienda);
-
-module.exports = router;
