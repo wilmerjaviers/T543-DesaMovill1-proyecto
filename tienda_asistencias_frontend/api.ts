4a867c80b451fde9084e46d37c0d28d3ff855a49
makdiel <makdiel.tabora@hotmail.com>
2025-09-17 18:34:44 -0600

    commit
    page administrativo avaces finales

--- tienda_asistencias_frontend/api.ts
+++ tienda_asistencias_frontend/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 const api = axios.create({ 
-  baseURL: "http://192.168.10.184:5000/api", 
+
+  baseURL: "http://192.168.10.105:5000/api", 
+
   timeout: 5000 
 });
 export default api;
